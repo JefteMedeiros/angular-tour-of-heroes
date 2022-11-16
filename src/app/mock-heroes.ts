fae5f96c0b278211eabfbc9a2900866f6f857424
Jefté Medeiros <jefte.medeiros8@gmail.com>
2022-11-16 15:50:52 -0500

    first-commit
    added heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Simple' },
+  { id: 13, name: 'FalleN' },
+  { id: 14, name: 'fer' },
+  { id: 15, name: 'coldzera' },
+  { id: 16, name: 'TACO' },
+  { id: 17, name: 'fnx' },
+  { id: 18, name: 'boltz' },
+  { id: 19, name: 'vini' },
+  { id: 20, name: 'arT' }
+];
